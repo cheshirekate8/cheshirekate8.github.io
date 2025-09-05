355cfcef307307a60db6377c772c93443a05167f
Katie Young <katie@sandstone.la>
2025-09-05 15:21:13 -0700

    removing docx for now

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -81,14 +81,14 @@ export default function Resume() {
           >
             DOWNLOAD PDF
           </a>
-          <a
+          {/* <a
             className="border bg-zinc-800 rounded-full px-6 py-2 font-jost font-bold w-max justify-center ease-in-out duration-200 drop-shadow-hard -translate-x-1 -translate-y-1 active:translate-x-0 active:translate-y-0 active:drop-shadow-none hover:bg-sky-900 md:text-lg md:px-12 md:py-4"
             href="KatieYoungResume.docx"
             target="_blank"
             download
           >
             DOWNLOAD DOCX
-          </a>
+          </a> */}
         </div>
         <div className="flex flex-wrap items-start">
           <div className="w-full md:w-1/2 px-5 pb-16">
